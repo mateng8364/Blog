54fcc1ab9ccc6a8bbbd06e62e1f9fd55b9392abf
mateng8364 <mateng8364@163.com>
2021-09-14 09:11:18 +0800

    project init 20210914

---
+++ axios/AxiosInterceptors.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+
+const CancelToken = axios.CancelToken
+const axiosRequestTaskQueue = [];
+const CANCEL_MESSAGE = 'INTERCEPT'
+const __removeRequestTaskQueue = function (type) {
+  if (!type) return undefined
+  let result = false
+  for (let i = 0; i < axiosRequestTaskQueue.length; i++) {
+    if (axiosRequestTaskQueue[i].type === type) {
+      axiosRequestTaskQueue.splice(i, 1)
+      result = true
+      break
+    }
+  }
+  return result
+}
+
+const __addRequestTaskQueue = function (type, cancel, message) {
+  for (let i = 0; i < axiosRequestTaskQueue.length; i++) {
+    if (axiosRequestTaskQueue[i].type === type) {
+      return cancel(message)
+    }
+  }
+  axiosRequestTaskQueue.push({cancel, type})
+}
+
+export const clearRequestTaskQueue = function () {
+  axiosRequestTaskQueue.forEach(item => {
+    item.cancel && item.cancel(CANCEL_MESSAGE)
+  })
+  axiosRequestTaskQueue.length = 0
+}
+
+class AxiosInterceptors {
+  static requestInterceptor(config) {
+    const type = config.baseURL + config.url + JSON.stringify(config.data)
+    let cancel
+    config.cancelToken = new CancelToken(c => cancel = c)
+    __addRequestTaskQueue(type, cancel, `${config.url} intercept`)
+    // 添加公共参数
+    return config
+  }
+
+  static requestErrorInterceptor(error) {
+    return Promise.reject(error)
+  }
+
+  static responseInterceptor(res) {
+    const type = res.config.url + res.config.data
+    __removeRequestTaskQueue(type)
+    // 添加统一处理
+    return res.data
+  }
+
+  static responseErrorInterceptor(error) {
+    if (axios.isCancel(error)) {
+      return 
+    }
+    if (error.config) {
+      const type = error.config.url + error.config.data
+      __removeRequestTaskQueue(type)
+    }
+    // 添加错误统一处理
+    return Promise.reject(error)
+  }
+}
+
+export default AxiosInterceptors
